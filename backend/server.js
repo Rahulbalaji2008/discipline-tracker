082ba1461495463bfb315040aa1fe6da45414429
Rahulbalaji2008 <rahulbalaji2008@gmail.com>
2026-04-13 17:52:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use('/api/friends', friendRoutes);
 
 // Database Connection
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/discipline-tracker';
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
   .then(() => {
